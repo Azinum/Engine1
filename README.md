6079ee84bc89d249cd8d04d219b2304f7847f04b
azinum <lukaska61@gmail.com>
2019-04-17 15:48:57 +0200

    Update README

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Engine 1
 
 Game engine in C++ and OpenGL.
+
+
+## DEPENDENCIES
+
+- OpenGL
+- LibPNG
+- SDL2
+- GLEW
+- GLM
--- include/Engine/Entity.h
+++ include/Engine/Entity.h
@@ -3,8 +3,19 @@
 #ifndef _ENTITY_H
 #define _ENTITY_H
 
-struct Entity {
+struct Vector3f {
+  float x, y, z;
+};
 
+struct Transform {
+  struct Vector3f position;
+  struct Vector3f rotation;
+  struct Vector3f scale;
+};
+
+struct Entity {
+  struct Transform transform;
+  int modelId;
 };
 
 #endif // _ENTITY_H
